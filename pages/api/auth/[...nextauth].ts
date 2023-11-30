d80cf3c024ecb4cae1dd90e23595cc621e266994
hirenf14 <hirenf14@gmail.com>
2023-11-30 14:55:47 +0530

    feat: added github pat auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import { Octokit } from "octokit";
 import CredentialProvider from "next-auth/providers/credentials";
 
-export const authOptions = {
-  secret: process.env.AUTH_SECRET,
+export const authOptions: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialProvider({
       name: "Credentials",
@@ -17,10 +17,11 @@ export const authOptions = {
         if (data) {
           return {
             ...data,
+            personalAccessToken: credentials?.personalAccessToken,
             id: data.login,
             name: data.name,
             email: data.email,
-            avatar: data.avatar_url,
+            image: data.avatar_url,
           };
         } else {
           return null;
@@ -28,5 +29,20 @@ export const authOptions = {
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, account, user }) {
+      // TODO: Find a Typesafe way to this.
+      const userInfo = user as any;
+      if(account) {
+        token.accessToken = userInfo.personalAccessToken
+      }
+      return token
+    }
+  },
+  pages: {
+    signIn: '/login',
+    error: '/error'
+  }
+
 };
 export default NextAuth(authOptions);
