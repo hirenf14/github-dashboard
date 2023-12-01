16d40807b57bcb2ab48d691ac8b968b2aca663f7
hirenf14 <hirenf14@gmail.com>
2023-12-01 13:26:55 +0530

    feat: added table with react table

---
+++ src/components/table/index.tsx
@@ -0,0 +1,140 @@
+import React from "react";
+import { useTable } from "@refinedev/react-table";
+import { ColumnDef, flexRender } from "@tanstack/react-table";
+import { List, usePagination } from "@refinedev/chakra-ui";
+import {
+    TableContainer,
+    Table,
+    Thead,
+    Tr,
+    Th,
+    Tbody,
+    Td,
+    HStack,
+    Button,
+    IconButton,
+    Box,
+} from "@chakra-ui/react";
+import { IconChevronRight, IconChevronLeft } from "@tabler/icons";
+
+interface IResourceTableProps {
+    columns: ColumnDef<any>[]
+}
+
+const ResourceTable: React.FC<IResourceTableProps> = ({ columns }) => {
+
+    const {
+        getHeaderGroups,
+        getRowModel,
+        refineCore: {
+            setCurrent,
+            pageCount,
+            current,
+        },
+    } = useTable({
+        columns,
+    });
+
+    return (
+        <List>
+            <TableContainer whiteSpace="pre-line">
+                <Table variant="simple">
+                    <Thead>
+                        {getHeaderGroups().map((headerGroup) => (
+                            <Tr key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <Th key={header.id}>
+                                        {!header.isPlaceholder &&
+                                            flexRender(
+                                                header.column.columnDef.header,
+                                                header.getContext(),
+                                            )}
+                                    </Th>
+                                ))}
+                            </Tr>
+                        ))}
+                    </Thead>
+                    <Tbody>
+                        {getRowModel().rows.map((row) => (
+                            <Tr key={row.id}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <Td key={cell.id}>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext(),
+                                        )}
+                                    </Td>
+                                ))}
+                            </Tr>
+                        ))}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+            <Pagination
+                current={current}
+                pageCount={pageCount}
+                setCurrent={setCurrent}
+            />
+        </List>
+    );
+};
+
+type PaginationProps = {
+    current: number;
+    pageCount: number;
+    setCurrent: (page: number) => void;
+};
+
+const Pagination: React.FC<PaginationProps> = ({
+    current,
+    pageCount,
+    setCurrent,
+}) => {
+    const pagination = usePagination({
+        current,
+        pageCount,
+    });
+
+    return (
+        <Box display="flex" justifyContent="flex-end">
+            <HStack my="3" spacing="1">
+                {pagination?.prev && (
+                    <IconButton
+                        aria-label="previous page"
+                        onClick={() => setCurrent(current - 1)}
+                        disabled={!pagination?.prev}
+                        variant="outline"
+                    >
+                        <IconChevronLeft size="18" />
+                    </IconButton>
+                )}
+
+                {pagination?.items.map((page) => {
+                    if (typeof page === "string")
+                        return <span key={page}>...</span>;
+
+                    return (
+                        <Button
+                            key={page}
+                            onClick={() => setCurrent(page)}
+                            variant={page === current ? "solid" : "outline"}
+                        >
+                            {page}
+                        </Button>
+                    );
+                })}
+                {pagination?.next && (
+                    <IconButton
+                        aria-label="next page"
+                        onClick={() => setCurrent(current + 1)}
+                        variant="outline"
+                    >
+                        <IconChevronRight size="18" />
+                    </IconButton>
+                )}
+            </HStack>
+        </Box>
+    );
+};
+
+export default ResourceTable;
