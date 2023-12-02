9809ab6857c348847b08d0f14a51384905b8c356
hirenf14 <hirenf14@gmail.com>
2023-12-02 18:08:19 +0530

    feat: dashboard card with action

--- src/components/cards/dashboard-card.tsx
+++ src/components/cards/dashboard-card.tsx
@@ -3,27 +3,39 @@ import {
   CardBody,
   CardHeader,
   CardProps,
+  Flex,
   Heading,
   useColorModeValue,
 } from "@chakra-ui/react";
 import * as React from "react";
 
 interface IDashboardCardProps extends CardProps {
-  title: string;
+  title?: string;
+  actions?: React.ReactNode;
 }
 
 export const DashboardCard: React.FunctionComponent<IDashboardCardProps> = ({
   title,
   children,
+  actions,
   ...props
 }) => {
   const bgColor = useColorModeValue("white", "gray.800");
   const titleBgColor = useColorModeValue("gray.100", "gray.700");
   return (
     <Card overflow="hidden" bgColor={bgColor} {...props}>
-      <CardHeader py={3} bgColor={titleBgColor}>
-        <Heading fontSize="lg" fontWeight={600}>{title}</Heading>
-      </CardHeader>
+      {title && (
+        <CardHeader py={3} bgColor={titleBgColor}>
+          <Flex gap={2}>
+            <Flex flex={1} flexWrap="wrap">
+              <Heading fontSize="lg" fontWeight={600}>
+                {title}
+              </Heading>
+            </Flex>
+            {actions}
+          </Flex>
+        </CardHeader>
+      )}
       <CardBody p={2}>{children}</CardBody>
     </Card>
   );
