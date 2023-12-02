a83a5b830e247e836977b45531fffc069659a6e8
hirenf14 <hirenf14@gmail.com>
2023-12-02 20:27:14 +0530

    feat: custom page title

--- src/components/page-title/index.tsx
+++ src/components/page-title/index.tsx
@@ -1,19 +1,46 @@
+import { useRouterContext, useRouterType, useLink } from "@refinedev/core";
+import { Link as ChakraLink, HStack, Heading } from "@chakra-ui/react";
 import { AppIcon } from "@components/app-icon";
-import {
-  RefineLayoutThemedTitleProps,
-  ThemedTitleV2,
-} from "@refinedev/chakra-ui";
+import { RefineLayoutThemedTitleProps } from "@refinedev/chakra-ui";
 import React from "react";
 
-export const PageTitle: React.FC<Partial<RefineLayoutThemedTitleProps>> = (
-  props
-) => {
+export const PageTitle: React.FC<Partial<RefineLayoutThemedTitleProps>> = ({
+  collapsed,
+  text = "Github Refined",
+  wrapperStyles,
+}) => {
+  const routerType = useRouterType();
+  const Link = useLink();
+  const { Link: LegacyLink } = useRouterContext();
+
+  const ActiveLink = routerType === "legacy" ? LegacyLink : Link;
+
   return (
-    <ThemedTitleV2
-      collapsed={false}
-      text="Github Dashboard"
-      icon={<AppIcon />}
-      {...props}
-    />
+    <ChakraLink
+      as={ActiveLink}
+      to="/"
+      fontSize="inherit"
+      textDecoration="none"
+      _hover={{
+        textDecoration: "none",
+      }}
+    >
+      <HStack
+        spacing="8px"
+        justifyContent="center"
+        alignItems="center"
+        fontSize="inherit"
+        style={{
+          ...wrapperStyles,
+        }}
+      >
+        <AppIcon />
+        {!collapsed && (
+          <Heading as="h6" fontWeight={700} fontSize="inherit">
+            {text}
+          </Heading>
+        )}
+      </HStack>
+    </ChakraLink>
   );
 };
