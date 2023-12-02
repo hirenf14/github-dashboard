2861f20f88a8985861d0bd86bba619b486b472d1
hirenf14 <hirenf14@gmail.com>
2023-12-02 14:05:36 +0530

    feat: added dashboard card

---
+++ src/components/cards/dashboard-card.tsx
@@ -0,0 +1,30 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardProps,
+  Heading,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import * as React from "react";
+
+interface IDashboardCardProps extends CardProps {
+  title: string;
+}
+
+export const DashboardCard: React.FunctionComponent<IDashboardCardProps> = ({
+  title,
+  children,
+  ...props
+}) => {
+  const bgColor = useColorModeValue("white", "gray.800");
+  const titleBgColor = useColorModeValue("gray.100", "gray.700");
+  return (
+    <Card overflow="hidden" bgColor={bgColor} {...props}>
+      <CardHeader py={2} bgColor={titleBgColor}>
+        <Heading fontSize="xl">{title}</Heading>
+      </CardHeader>
+      <CardBody p={2}>{children}</CardBody>
+    </Card>
+  );
+};
