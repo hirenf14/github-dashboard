52952ccfe6f8b16daeb19a763c867b141736f0ed
hirenf14 <hirenf14@gmail.com>
2023-12-02 20:33:11 +0530

    feat: bypass middleware for images

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,6 @@ export const config = {
        * - _next/image (image optimization files)
        * - favicon.ico (favicon file)
        */
-      '/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)',
+      '/((?!api/auth|_next/static|_next/image|favicon.ico|login|images).*)',
     ],
   }
