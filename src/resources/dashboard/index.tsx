9a7d2a578e0ff4d5b38bf768820c3a0f8e5d7d9e
hirenf14 <hirenf14@gmail.com>
2023-12-02 14:23:53 +0530

    feat: added dashboard ui

--- src/resources/dashboard/index.tsx
+++ src/resources/dashboard/index.tsx
@@ -1,13 +1,31 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { MemeCard } from "@components/meme-card";
-import { IResourceComponentsProps } from "@refinedev/core";
-import React from "react";
+import { temp } from "@config/temp";
+import { IResourceComponentsProps, useList } from "@refinedev/core";
+import React, { useMemo } from "react";
+import { CommitHistoryChart } from "./charts/CommitHistoryChart";
+import { DashboardCard } from "@components/cards";
+import { formatHistory } from "@helpers/formatHistory";
+import { ContributionWeek } from "@interfaces/contributions";
 
 export const Dashboard: React.FC<IResourceComponentsProps> = () => {
+  const { data } = useList<ContributionWeek>({ resource: "stats" });
+  const history = useMemo(() => {
+    return formatHistory(data?.data || []);
+  }, [data]);
   return (
-    <Grid gridTemplateColumns="repeat(4, 1fr)">
-      <GridItem>
-        <MemeCard maxHeight="100%" height="auto" />
+    <Grid gridTemplateColumns="repeat(12, 1fr)" gap={4}>
+      <GridItem colSpan={7}>
+        <DashboardCard title="Contributions last 6 months">
+          <Box height={400}>
+            <CommitHistoryChart history={history} />
+          </Box>
+        </DashboardCard>
+      </GridItem>
+      <GridItem colSpan={5}>
+        <DashboardCard title="Random Meme">
+          <MemeCard maxH={400} m="auto" objectFit="contain" />
+        </DashboardCard>
       </GridItem>
     </Grid>
   );
